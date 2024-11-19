835d9c93a0519d15a84e31471530504d13fe96d4
Martin A. Ortega <lynxestudio@outlook.com>
2024-11-19 10:00:36 -0600

    Add source code to the project

---
+++ src/FrmQuery/FrmQuery.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmQuery
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCommand = New System.Windows.Forms.TextBox()
+        Me.btnRun = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txtCommand
+        '
+        Me.txtCommand.Dock = System.Windows.Forms.DockStyle.Top
+        Me.txtCommand.Location = New System.Drawing.Point(0, 0)
+        Me.txtCommand.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtCommand.Multiline = True
+        Me.txtCommand.Name = "txtCommand"
+        Me.txtCommand.ReadOnly = True
+        Me.txtCommand.Size = New System.Drawing.Size(542, 51)
+        Me.txtCommand.TabIndex = 0
+        Me.txtCommand.Text = "SELECT COUNT(*) FROM Production.Product"
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(14, 70)
+        Me.btnRun.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(99, 30)
+        Me.btnRun.TabIndex = 1
+        Me.btnRun.Text = "Run"
+        Me.btnRun.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.txtOutput.Location = New System.Drawing.Point(0, 108)
+        Me.txtOutput.Multiline = True
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(542, 147)
+        Me.txtOutput.TabIndex = 2
+        '
+        'FrmQuery
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(542, 255)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnRun)
+        Me.Controls.Add(Me.txtCommand)
+        Me.Font = New System.Drawing.Font("Arial", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MaximizeBox = False
+        Me.Name = "FrmQuery"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ExecuteScalar"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCommand As System.Windows.Forms.TextBox
+    Friend WithEvents btnRun As System.Windows.Forms.Button
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+
+End Class
---
+++ src/FrmQuery/FrmQuery.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+
+
+Public Class FrmQuery
+
+    Private Sub btnRun_Click(sender As System.Object, e As System.EventArgs) Handles btnRun.Click
+        Try
+            Dim connectionString As String = ConfigurationManager.ConnectionStrings("advWorks").ConnectionString
+            Dim connection As New SqlConnection(connectionString)
+            Dim command As New SqlCommand(txtCommand.Text, connection)
+            command.CommandType = CommandType.Text
+            connection.Open()
+            Dim count As Integer = CInt(command.ExecuteScalar())
+            connection.Close()
+            txtOutput.Text = "Executing the method ExecuteScalar()"
+            txtOutput.Text = "Total of products " + count.ToString()
+        Catch ex As ApplicationException
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
